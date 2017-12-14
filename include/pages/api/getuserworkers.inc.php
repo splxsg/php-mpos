e0c716350833cba91ee8ba1662154840bcb41059
splxsg <splxsg@gmail.com>
2017-12-14 00:36:37 +0000

    v

--- include/pages/api/getuserworkers.inc.php
+++ include/pages/api/getuserworkers.inc.php
@@ -1,12 +1,12 @@
 <?php
-$defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
+//$defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
 
 // Check if the API is activated
-$api->isActive();
+//$api->isActive();
 
 // Check user token
 //$user_id = $api->checkAccess($user->checkApiKey($_REQUEST['api_key']), @$_REQUEST['id']);
-$user_id =  "1";
+$user_id =  @$_REQUEST['id'];
 // Fetch data interval from admin settings
 if ( ! $interval = $setting->getValue('statistics_ajax_data_interval')) $interval = 300;
 
