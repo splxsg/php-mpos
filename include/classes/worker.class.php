90a557c6d69396ff51fe0767c4a75539a072f985
splxsg <splxsg@gmail.com>
2017-12-14 00:46:41 +0000

    v

--- include/classes/worker.class.php
+++ include/classes/worker.class.php
@@ -1,5 +1,5 @@
 <?php
-$defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
+//$defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
 
 class Worker extends Base {
   protected $table = 'pool_worker';
